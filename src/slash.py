e68c3d8510e1bcd591bea6e96421663308c666a7
Bobby Pinard <bobby.pinard@gmail.com>
2023-11-25 15:42:18 -0600

    working but as a list of lists

---
+++ src/slash.py
@@ -0,0 +1,43 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from config import TOKEN
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents = intents)
+
+restaurants = [
+    ['Restaurant1', 'Cuisine1', 'Address1'],
+    ['Restaurant2', 'Cuisine2', 'Address2'],
+    ['Restaurant3', 'Cuisine3', 'Address3']
+]
+
+def format(arr):
+    output = ''
+    for restaurant, cuisine, address in arr:
+        output += '- ' + restaurant + ' - ' + cuisine + ' - ' + address + '\n'
+    output += ''
+    return output
+
+@bot.event
+async def on_ready():
+    print("Bot is up and ready")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name = "add", description = "Add restaurant to the Houston foods database")
+async def add(interaction: discord.Interaction, restaurant: str, cuisine: str, address: str):
+    lineItem = [restaurant, cuisine, address]
+    restaurants.append(lineItem)
+    await interaction.response.send_message(f'Added {restaurant} to the database')
+
+@bot.tree.command(name = "list", description = "List out all restaurants in the Houston foods database")
+async def list(interaction: discord.Interaction):
+    output = format(restaurants)
+    await interaction.response.send_message(output)
+
+bot.run(TOKEN)
